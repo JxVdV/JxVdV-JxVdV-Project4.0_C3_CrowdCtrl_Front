87feb23124f7f38c7bb1df0a52c6a1f6043303c4
JxVdV <vandeveljari@outlook.com>
2023-01-18 13:50:35 +0100

    pipeline preps

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import ApiDetailsView from '../views/ApiDetails.vue';
+
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: HomeView,
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    name: 'About',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/api',
+    name: 'Api',
+    component: () => import('../views/ApiView.vue')
+  },
+  {
+    path: '/api/:id',
+    name: 'ApiDetails',
+    component: () => import('../views/ApiDetails.vue'),
+    props: true,
+  },
+  {
+    path: '/shouldRedirectToApi',
+    redirect: '/api',
+  },
+  // catchAll 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: () => import('../views/NotFound.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+  linkActiveClass: 'active'
+});
 
-export default router
+export default router;
